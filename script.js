856990a8f6fecfdbcadd9a5f342739ec2400431e
ky213 <y.keddaoui@gmail.com>
2018-04-27 21:49:59 +0100

    finish

--- script.js
+++ script.js
@@ -1,62 +1,65 @@
-// Send a request
+const channels = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas"]
+const stream_modal = $('.stream')
 
-$("form").on("submit", function () {
-    const type = $('label.active input').attr('name');
-    const query = $('#search').val();
-    const url = "https://wind-bow.glitch.me/twitch-api/" + type + "/" + encodeURI(query);
+channels.forEach(function (query) {
+    fetch('https://wind-bow.glitch.me/twitch-api/streams/' + query)
+        .then(async function (data) {
+            displayStreams(await data.json(), query);
+        }).catch(function (err) {
+            console.log("An error Happend: ", error);
+        })
+})
 
-    getData(url, type);
 
-    return false
+
+function displayStreams(streams, query) {
+    const stream = streams.stream;
+    const new_stream = $('.stream').first().clone().attr('id', query)
+
+    new_stream.removeClass('d-none')
+        .find('.display-name')
+        .text(query)
+
+
+    if (streams.stream) {
+        new_stream
+            .attr('href', stream.channel.url)
+            .css('border-left', '5px solid var(--green)')
+            .find('.followers')
+            .text('Followers: ' + stream.channel.followers)
+        new_stream.find('.viewers')
+            .text('Viewers: ' + stream.viewers)
+        new_stream.find('.status')
+            .text('Status: Online')
+        new_stream.find('.logo')
+            .attr('src', stream.preview.large)
+
+        new_stream.appendTo('.all, .online')
+
+    } else {
+        new_stream
+            .appendTo('.all, .offline')
+            .css('border-left', '5px solid var(--red)')
+
+    }
+}
+
+
+
+$("#filter").on("keyup", function () {
+    const filter = this.value
+    const exp = new RegExp('^' + filter, 'i')
+
+    if (filter) {
+        $("div.show")
+            .children()
+            .filter(function () {
+                return !this.id.match(exp)
+            })
+            .addClass('d-none')
+    } else {
+        $("div.show")
+            .children('[id]')
+            .removeClass('d-none')
+    }
 });
-
-function getData(url, type) {
-    spinner('add')
-    fetch(url).then(async function (res) {
-        spinner('remove')
-        type === "streams" ?
-            displayStreams(await res.json())
-            :
-            displayChannels(await res.json())
-
-    }).catch(function (err) {
-        spinner('remove')
-        console.log("Error Happend: \n" + err);
-    })
-}
-
-function displayStreams(streams) {
-    const results = $('#results');
-
-    results.empty()
-
-    if (!streams.stream) {
-        const msg = $("<h1 class='display-1'>No Stream :(</h1>")
-        results.append(msg);
-    } else {
-        $.each(stream, function (i, v) {
-            console.log(v);
-        });
-    }
-}
-
-function displayChannels(channels) {
-    const results = $('#results');
-    const chan = $('<a></a>');
-
-    results.empty()
-    console.log(channels);
-}
-
-function spinner(action) {
-    const btnText = $('button[type="submit"] span');
-    const spinner = $('.fa-spinner');
-
-    if (action === 'add') {
-        spinner.removeClass('d-none')
-        btnText.text('')
-    } else {
-        spinner.addClass('d-none')
-        btnText.text('Go!')
-    }
-}
